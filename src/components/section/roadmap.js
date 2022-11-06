a87c1da22bb22719e745cbb216d8a6914b5add4c
Your Name <you@example.com>
2022-11-06 14:55:36 -0700

    updates per clients request

--- src/components/section/roadmap.js
+++ src/components/section/roadmap.js
@@ -28,39 +28,19 @@ export function Roadmap() {
                        
             </p>
           </div>
-          <div className='roadmap'><p style={{ fontSize: '15px', textAlign:'start', fontFamily:'Montserrat'}}>Artist Headhunting & Twitter Influencers</p>
-          <p style={{textAlign:'start', paddingRight:'25px',fontFamily:'Montserrat'}}>
-            Comeptitions, eliminations & recruitment.
-            Awarded best art designs & signed contracts with abolutley THE BEST artists and architects.
-            Cosmosapiens NFT Base layer design and features. Fisrt art engine run!
-            Onboarding an Architect to our team to develop first virtual PADs/Offices for our international team to colab in.
-          </p>
-          </div>
-          <div className='roadmap'><p style={{ fontSize: '15px', textAlign:'end', fontFamily:'Montserrat'}}>Test - Deploy - Launch and Lock</p>
-            <p style={{textAlign:'end', paddingLeft:'25px'}}>The $Cometa token will be used as an internal currency within our platform. 
+
+          <div className='roadmap'><p style={{ fontSize: '15px', textAlign:'start', fontFamily:'Montserrat'}}>Test - Deploy - Launch and Lock</p>
+            <p style={{textAlign:'start', paddingLeft:'25px'}}>The $x token will be used as an internal currency within our platform. 
               Each token holder will receive voting rights correspondingly to their token share and will be able to 
               directly participate in operational management and strategic development of the entire project.
             </p>
           </div>
-          <div className='roadmap' ><p style={{ fontSize: '15px', textAlign:'start', fontFamily:'Montserrat'}}>Community Comeptitions, Giveaways and Airdrops</p>
-            <p style={{textAlign:'start', paddingRight:'25px',fontFamily:'Montserrat'}}>Foster our vibrant community of Cometa Club & Cosmosapiens. 
-            All of holders will have exclusive access to our Cosmic Virtual Reality PODs
-            Early $Cometa and Cosmosapiens investors will reseve an a limited Cosmosapien Companion - Doge Fren NFT and well as Doge token airdops.
-            To our loyal holders and supporters we will gift custom designed by our inhouse architects, metaverse VR pods - yours to own on the blockchain.
+          <div className='roadmap'><p style={{fontSize: '15px', textAlign:'end', fontFamily:'Montserrat'}}>Investor HUB</p>
+            <p style={{textAlign:'end', paddingRight:'25px'}}>Launch of our Investor's HUB platform and private ERC-721 estates.
             </p>
           </div>
-          <div className='roadmap'><p style={{fontSize: '15px', textAlign:'end', fontFamily:'Montserrat'}}>Certik Audit & KYC</p>
-            <p style={{textAlign:'end', paddingLeft:'25px',fontFamily:'Montserrat'}}>The $cometa token is deployed on a standardized, secured and verified BEP20 rewards
-            smart contract. We released $cometa after extensive penetration testing and we are confident in the contract security. Certik results will be posted here on our website
-             and on our social channels.
-            </p>
-          </div>
-          <div className='roadmap'><p style={{fontSize: '15px', textAlign:'start', fontFamily:'Montserrat'}}>Investor HUB</p>
-            <p style={{textAlign:'start', paddingRight:'25px'}}>Launch of our Investor's HUB platform and private ERC-721 estates.
-            </p>
-          </div>
-          <div className='roadmap'><p style={{fontSize: '15px', textAlign:'end',fontFamily:'Montserrat'}}>Opensource Charity Voting</p>
-            <p style={{textAlign:'end', paddingLeft:'25px',fontFamily:'Montserrat'}}>Not leaving others behind is the fundation of our civilization.
+          <div className='roadmap'><p style={{fontSize: '15px', textAlign:'start',fontFamily:'Montserrat'}}>Opensource Charity Voting</p>
+            <p style={{textAlign:'start', paddingLeft:'25px',fontFamily:'Montserrat'}}>Not leaving others behind is the fundation of our civilization.
             Opensource donations to charities and fundations of community choosing.
             </p>
           </div>
@@ -69,7 +49,7 @@ export function Roadmap() {
               We value our community above all else and want all of you to be a big part of moving the project forward in the future.
               
               Our aim is to be far more than just a Investment Token project, we are delivering purpose and value to our community long term.
-              More importantly we want to be make significant contributions toward moving humanity forward. Hholders will be considered part of the Cosmosapiens POD and have access to exclusive 
+              More importantly we want to be make significant contributions toward moving humanity forward. Hholders will be considered part of the HUB and have access to exclusive 
               airdrops, prizes, raffles, and future collections. </p>
             </div>
           </div>
